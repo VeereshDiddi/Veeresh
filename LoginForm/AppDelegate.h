c6b102e005bb89e7223e5645ac7d08dbd3c04df7
MacBook Pro <srinivasreddykatta@Srinivass-MacBook-Pro.local>
2016-11-02 14:49:11 +0530

    first time commiting

---
+++ LoginForm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoginForm
+//
+//  Created by MacBook Pro on 09/08/16.
+//  Copyright © 2016 MacBook Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
